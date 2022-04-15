09ed87557ab90555aecfee3795ea28b33d85444b
Ninitprog <chanrion.nicolas@gmail.com>
2022-04-15 11:31:35 +0200

    creation des routes

--- src/Controller/TacheController.php
+++ src/Controller/TacheController.php
@@ -17,4 +17,49 @@ class TacheController extends AbstractController
             'controller_name' => 'TacheController',
         ]);
     }
+    /**
+     * @Route("/ListeTaches", name="liste")
+     */
+    public function ListeTaches(): Response
+    {
+        return $this->render('tache/index.html.twig', [
+            'controller_name' => 'TacheController',
+        ]);
+    }
+    /**
+     * @Route("/tache", name="app_tache")
+     */
+    public function taches(): Response
+    {
+        return $this->render('tache/index.html.twig', [
+            'controller_name' => 'TacheController',
+        ]);
+    }
+    /**
+     * @Route("/suprTaches", name="supr")
+     */
+    public function suprTaches(): Response
+    {
+        return $this->render('tache/index.html.twig', [
+            'controller_name' => 'TacheController',
+        ]);
+    }
+    /**
+     * @Route("/creerTaches", name="crer")
+     */
+    public function creerTaches(): Response
+    {
+        return $this->render('tache/index.html.twig', [
+            'controller_name' => 'TacheController',
+        ]);
+    }
+    /**
+     * @Route("/inserTaches", name="inser")
+     */
+    public function inserTaches(): Response
+    {
+        return $this->render('tache/index.html.twig', [
+            'controller_name' => 'TacheController',
+        ]);
+    }
 }
